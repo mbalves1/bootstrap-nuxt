60f3030980a9126ff0952a219c6d958480ac3bca
Murilo <mbalves1@outlook.com>
2023-11-28 20:34:55 -0300

    [feat]: profile page add name

--- components/ViewProfile.vue
+++ components/ViewProfile.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-center mt-4">
-    <v-card class="bg-white w-full rounded-lg" max-width="500">
+  <div class="flex flex-col items-center justify-center mt-4">
+    <v-card variant="flat" class="bg-white border w-full rounded-lg" max-width="500">
       <div class="font-bold text-base pa-5">Meus dados</div>
       <v-divider></v-divider>
       <v-card-text>
@@ -10,13 +10,15 @@
             <v-text-field
               variant="outlined"
               density="compact"
-            ></v-text-field>
-            <div>Email</div>
-            <v-text-field
+              v-model="user.name"
+              ></v-text-field>
+              <div>Email</div>
+              <v-text-field
               variant="outlined"
               density="compact"
-            ></v-text-field>
-            <v-btn
+              v-model="user.email"
+              ></v-text-field>
+              <v-btn
               color="#00B8C5"
               variant="flat"
               class="w-full rounded-lg my-10 text-capitalize text-white"
@@ -26,7 +28,32 @@
         </v-form>
       </v-card-text>
     </v-card>
+
+    <div class="flex justify-between bg-white border rounded-lg sm:w-500px mt-7 pa-6">
+      <div>Senha</div>
+      <div class="flex items-center text-#00B8C5">
+        <v-icon class="mr-2">mdi-pencil-box-outline</v-icon>
+        Alterar
+      </div>
+    </div>
+
+    <div class="flex justify-between bg-white border rounded-lg sm:w-500px mt-7 pa-6">
+      <div class="flex items-center text-red">
+        <v-icon class="flex mr-2" style="transform: rotate(180deg);">mdi-login</v-icon>
+        <div class="font-bold text-red">Sair da minha conta</div>
+      </div>
+    </div>
   </div>
 </template>
-<script>
+<script setup>
+const user = ref({
+  name: '',
+  email: ''
+})
+
+onMounted(() => {
+  const userStore = JSON.parse(localStorage.getItem('user'))
+  user.value.name = userStore.name
+  user.value.email = userStore.email
+})
 </script>
